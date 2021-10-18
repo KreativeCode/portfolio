127e08eecbdc2e5cd752e271776274629dfff291
chris poalnco <chris.polanco2@gmail.com>
2021-10-18 19:34:57 -0400

    fixed global templates layout and files and setup admin for handling the Polls app database

--- portfolio-project/polls/views.py
+++ portfolio-project/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404,render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
